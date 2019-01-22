9314d5a5f6874519880be7d497d2be74b484b5a1
Tim <timothyolaleke@gmail.com>
2019-01-22 17:19:00 +0100

    Added more codes

--- messenger/urls.py
+++ messenger/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import home, signup, process_listen as SMSController, USSDController
+from .views import home, signup, process_listen as SMSController
     
 urlpatterns = [
     path('', home, name='home'),
     path('listener/',  SMSController, name='listen'),
-    path('ussd-listener/', USSDController.as_view(), name='ussd-listen'),
     path('signup/', signup, name='signup')
 ]
--- messenger/views.py
+++ messenger/views.py
@@ -11,14 +11,15 @@ from .forms import SignUpForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from ussd.core import UssdView, UssdRequest
-
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search
 
 # Homepage function
 @login_required(login_url='/accounts/login/')
 def home(request):
     return render(request, 'home.html', {})
 
-# Function for User signuo
+# Function for User signup
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
@@ -38,63 +39,33 @@ def signup(request):
 @require_POST
 def process_listen(request):
     incoming_data = request.POST.copy()
+    print(incoming_data)
     text_data = incoming_data.get('text')
     # Regex to search for email
-    email = re.search(r'[\w\.-]+@[\w\.-]+', text_data)
-    email = email.group(0)
+    # email = re.search(r'[\w\.-]+@[\w\.-]+', text_data)
+    # email = email.group(0)
+    email = ''
+    # Save received sms to Hooks
     hook = Hook(data=text_data, type='SMS')
     hook.save()
     subject = 'Messenger'
     message = text_data
     email_from = settings.EMAIL_HOST_USER
     recipient_list = [email,]
-    send_mail( subject, message, email_from, recipient_list )
+    # send_mail( subject, message, email_from, recipient_list )
     return HttpResponse("Email sent to {}".format(email))
 
-# Function that listens to USSD Webhooks and send Email
-class USSDController(UssdView):
-    customer_journey_namespace = "USSDController"
-    customer_journey_conf = settings.DEFAULT_USSD_SCREEN_JOURNEY
-    
-    def post(self, req):
-        list_of_inputs = req.data['text'].split("*")
-        text = "*" if len(list_of_inputs) >= 2 and list_of_inputs[-1] == "" and list_of_inputs[-2] == "" else list_of_inputs[
-            -1]
+def es_search():
+    s = Search(using=client, index=index_name)
+    s.update_from_dict({"size": 1000})
+    # OR
+    s = Search(using=client, index="poi-health-facility")
+    response = s.execute()
+    # OR
+    s = Search(using=client, index="poi-health-facility") \
+            .query("match", id=facility_id)
+    response = s.execute()
+    pass
 
-        session_id = req.data['sessionId']
-        if req.data.get('use_built_in_session_management', False):
-            session_id = None
-        ussd_request = UssdRequest(
-            phone_number=req.data['phoneNumber'].strip('+'),
-            session_id=session_id,
-            ussd_input=text,
-            service_code=req.data['serviceCode'],
-            language=req.data.get('language', 'en'),
-            use_built_in_session_management=req.data.get(
-                'use_built_in_session_management', False)
-        )
-        text_data = "{}".format(req.data.get('text'))
-        # Regex to search for email
-        email = re.search(r'[\w\.-]+@[\w\.-]+', text_data)
-        if (email):
-            email = email.group(0)
-            hook = Hook(data=text_data, type='USSD')
-            hook.save()
-            subject = 'Messenger'
-            message = text_data
-            email_from = settings.EMAIL_HOST_USER
-            recipient_list = [email,]
-            send_mail( subject, message, email_from, recipient_list )
-        return ussd_request
-
-    def ussd_response_handler(self, ussd_response):
-        if self.request.data.get('serviceCode') == 'test':
-            return super(USSDController, self).\
-                ussd_response_handler(ussd_response)
-        if ussd_response.status:
-            res = 'CON' + ' ' + str(ussd_response)
-            response = HttpResponse(res)
-        else:
-            res = 'END' + ' ' + str(ussd_response)
-            response = HttpResponse(res)
-        return response
+def social_post():
+    pass
