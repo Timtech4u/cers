c260011e332e089752e1a2292196122c2a8c8aef
Tim <timothyolaleke@gmail.com>
2019-01-27 03:00:47 +0100

    updates

--- messenger/urls.py
+++ messenger/urls.py
@@ -4,6 +4,6 @@ urlpatterns = [
     path('', home, name='home'),
     path('map/', mapV, name='map'),
     path('reports/', listV, name='list'),
-    path('listener/',  process_listen, name='listen'),
-    path('signup/', signup, name='signup')
+    path('listener/',  process_listen, name='listen')
+    # path('signup/', signup, name='signup')
 ]
